a5e79bc6aa95e0e7f1ffa7a0aeb3efb476daa670
sathninduk <sathnidukottage@gmail.com>
2021-12-26 10:12:02 +0530

    Initial commit

---
+++ models/Redhat.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const RedhatSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: false
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    status: {
+        type: Number,
+        required: true,
+        default: 1
+    }
+});
+
+const Redhat = mongoose.model("redhat", RedhatSchema);
+module.exports = Redhat;
